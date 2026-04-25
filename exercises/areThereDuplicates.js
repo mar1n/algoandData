214828bf906af9ebab687cdd3e936e0584e5e83c
mar1n <cykcykacz@gmail.com>
2026-04-25 15:27:41 +0100

    are there duplicate  pointer solution

--- exercises/areThereDuplicates.js
+++ exercises/areThereDuplicates.js
@@ -1,24 +1,47 @@
 function areThereDuplicates(...values) {
-    if(values.length === 0 ) {
-        return false;
+  if (values.length === 0) return false;
+  let left = 0;
+  let index = 0;
+  let counter = 0
+  while(counter < values.length ** values.length) {
+    if(values[left] === values[index] && left !== index) {
+        return true;
     }
-    let frequencyCounter = {}
-    for(let value of values.join('')) {
-        frequencyCounter[value] = (frequencyCounter[value] || 0) + 1
+    if(left === values.length - 1) {
+        left = 0
+        index++
+    } else {
+        left++
     }
-    for(let value in frequencyCounter) {
-        if(frequencyCounter[value] > 1) {
-            return true
-        }
-    }
-
-    return false
+    counter++
+  }
+  return false
 }
 
-console.log(areThereDuplicates())
-console.log(areThereDuplicates(1, 2, 3))
-console.log(areThereDuplicates(1, 2, 2))
-console.log(areThereDuplicates('a', 'b', 'c', 'a'))
+
+[1, 2, 3]
+    
+//console.log(areThereDuplicates());
+console.log(areThereDuplicates('a', 'b', 'c', 'a'));
+// console.log(areThereDuplicates(1, 2, 2));
+// console.log(areThereDuplicates("a", "b", "c", "a"));
 // areThereDuplicates(1, 2, 3) // false
-// areThereDuplicates(1, 2, 2) // true 
-// areThereDuplicates('a', 'b', 'c', 'a') // true 
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+
+// function areThereDuplicates(...values) {
+//     if(values.length === 0 ) {
+//         return false;
+//     }
+//     let frequencyCounter = {}
+//     for(let value of values.join('')) {
+//         frequencyCounter[value] = (frequencyCounter[value] || 0) + 1
+//     }
+//     for(let value in frequencyCounter) {
+//         if(frequencyCounter[value] > 1) {
+//             return true
+//         }
+//     }
+
+//     return false
+// }
