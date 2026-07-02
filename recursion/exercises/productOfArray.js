833118c05b3bd8e4b6bed450bc30e8942bd127c0
mar1n <cykcykacz@gmail.com>
2026-07-02 22:07:48 +0100

    product of array

---
+++ recursion/exercises/productOfArray.js
@@ -0,0 +1,9 @@
+function alertes(arr) {
+    console.log('arr.slice(1)', arr.slice(1))
+    if(arr.length === 0) return 1
+    console.log('alertes', arr)
+    console.log('arr', arr.length)
+    return arr[0] * alertes(arr.slice(1))
+}
+
+alertes([1,2,3, 10])
