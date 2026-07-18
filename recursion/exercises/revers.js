641eb032cbd68e6527030555ec311f0c867d03d4
mar1n <cykcykacz@gmail.com>
2026-07-18 18:02:59 +0100

    reverse string recursion

---
+++ recursion/exercises/revers.js
@@ -0,0 +1,12 @@
+var newStr = ''
+function reverse(str, str2 = ''){
+  // add whatever parameters you deem necessary - good luck!
+  let leng = str.length;
+    newStr += str2
+    const finallStr = newStr
+    if(leng === 0) {
+        newStr = ''
+        return finallStr
+}
+    return  reverse(str.slice(0, leng - 1), str.slice(leng - 1))
+}
