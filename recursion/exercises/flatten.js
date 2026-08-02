6327fa2e234911d9b04e62dbc1e6105a1cb80d00
mar1n <cykcykacz@gmail.com>
2026-08-02 18:33:49 +0100

    flatten array recursion

---
+++ recursion/exercises/flatten.js
@@ -0,0 +1,18 @@
+var result = [];
+function deleteArr(...arr) {
+    if (!arr.length) {
+        return result
+    }
+    if (Array.isArray(arr[0])) {
+        if (arr.length > 1) {
+            return deleteArr(...[...arr[0], ...arr.slice(1)])
+        }
+
+        return deleteArr(...arr[0])
+    }
+    result.push(arr[0])
+
+    return deleteArr(...arr.slice(1))
+}
+
+console.log(deleteArr(...[[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]))
