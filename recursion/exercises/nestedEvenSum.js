9f1bef21d77125a844f65e07683dd8b66b75ad4e
mar1n <cykcykacz@gmail.com>
2026-08-05 22:16:02 +0100

    recursion on Object

---
+++ recursion/exercises/nestedEvenSum.js
@@ -0,0 +1,72 @@
+var accumulator = 0;
+var someArr;
+
+function nestedEvenSum(obj, arr) {
+    // add whatever parameters you deem necessary - good luck!
+
+    if (arr.length) {
+        console.log('arr length')
+        someArr = arr
+    } else {
+        someArr = Object.values(obj)
+    }
+    //console.log('obj', obj)
+
+    if (Object.prototype.toString.call(someArr[0]) === '[object Object]') {
+        console.log('object', someArr[0])
+        console.log('return object values', [...Object.values(someArr[0])])
+        return nestedEvenSum(obj, [...Object.values(someArr[0])])
+    }
+    if (typeof someArr[0] === 'number') {
+        accumulator += someArr[0]
+        console.log('number', someArr[0])
+    }
+    console.log('accumulator', accumulator)
+    if (!someArr.slice(1).length)
+        return accumulator
+    return nestedEvenSum(obj, someArr.slice(1))
+    //return 'pause'
+}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: "yup"
+        }
+    }
+}
+
+var obj2 = {
+    a: 2,
+    b: {
+        b: 2,
+        bb: {
+            b: 3,
+            bb: {
+                b: 2
+            }
+        }
+    },
+    c: {
+        c: {
+            c: 2
+        },
+        cc: 'ball',
+        ccc: 5
+    },
+    d: 1,
+    e: {
+        e: {
+            e: 2
+        },
+        ee: 'car'
+    },
+};
+//console.log(Object.values(obj2))
+nestedEvenSum(obj1, []);
+// 6
+//nestedEvenSum(obj2); // 10
