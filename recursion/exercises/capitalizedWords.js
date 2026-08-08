27f70b444657e4d9d7919eb97d14bb7069af5ee7
mar1n <cykcykacz@gmail.com>
2026-08-08 19:00:21 +0100

    recursion capitaliz words in array

---
+++ recursion/exercises/capitalizedWords.js
@@ -0,0 +1,12 @@
+var limit = 0;
+
+function capitalizedWords (arr) {
+  // add whatever parameters you deem necessary - good luck!
+  if(limit === arr.length) return arr;
+  arr[limit] = arr[limit].toUpperCase()
+  limit++
+  return capitalizeWords(arr)
+}
+capitalizedWords(['i', 'am', 'learning', 'recursion']);
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
