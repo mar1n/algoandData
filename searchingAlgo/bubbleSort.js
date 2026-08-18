b77b7776a2e97e57a0e1ad7699a228461129d14b
mar1n <cykcykacz@gmail.com>
2026-08-18 21:43:22 +0100

    bubble sort optimiasation

--- searchingAlgo/bubbleSort.js
+++ searchingAlgo/bubbleSort.js
@@ -32,3 +32,23 @@ function bubbleSort(arr) {
 bubbleSort([8,1,2,3,4,5,6,7]);
 
 //console.log(bubbleSort([37, 45, 29, 8]))
+
+//optimisation
+function bubbleSortOPT(arr){
+  var noSwaps;
+  for(var i = arr.length; i > 0; i--){
+    noSwaps = true;
+    for(var j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;         
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSortOPT([8,1,2,3,4,5,6,7]);
