b0334739adda5d0df6c346d3fa252e26952e299e
mar1n <cykcykacz@gmail.com>
2026-08-29 20:07:47 +0100

    singly linked list pop method

--- dataStructure/single linked list/singlyLinkedList.js
+++ dataStructure/single linked list/singlyLinkedList.js
@@ -26,8 +26,35 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    // traverse() {
+    //      var current = this.head;
+    //     var newTail = current;
+    //     while(current){
+    //         console.log(current.val)
+    //         newTail = current;
+    //         current = current.next;
+    //     }
+    // }
+    pop() {
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 
 var list = new SinglyLinkedList()
-// list.push("HELLO");
-// list.push("GOODBYE")
+list.push("HELLO");
+list.push("GOODBYE")
+list.push("!")
