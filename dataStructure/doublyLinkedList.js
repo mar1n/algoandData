aa0085675190debc1c81129050106d1d70fe8c46
mar1n <cykcykacz@gmail.com>
2026-09-02 21:55:54 +0100

    doubly Linked link set

--- dataStructure/doublyLinkedList.js
+++ dataStructure/doublyLinkedList.js
@@ -89,6 +89,14 @@ class DoublyLinkedList {
         }
         
     }
+    set(index, val) {
+        var foundNode = this.get(index);
+        if(foundNode != null) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
 var list = new DoublyLinkedList()
