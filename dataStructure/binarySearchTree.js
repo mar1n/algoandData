5f7da17b7bfbea627c9a6a7ddf12d191feedf121
mar1n <cykcykacz@gmail.com>
2026-09-13 11:34:49 +0100

    tree traversal depth first preOrder DFP

--- dataStructure/binarySearchTree.js
+++ dataStructure/binarySearchTree.js
@@ -66,13 +66,24 @@ class BinarySearchTree {
         }
         return data;
     }
+    DFSPreOrder() {
+        var data = [];
+        var current = this.root;
+        function traverse(node) {
+            data.push(node);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right)
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 var tree = new BinarySearchTree();
 tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+tree.DFSPreOrder()
