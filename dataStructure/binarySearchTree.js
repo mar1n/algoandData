8e29fce065b170f4f1ed929fbe6bd3decf107d98
mar1n <cykcykacz@gmail.com>
2026-09-13 10:50:46 +0100

    tree traversal breadth first search BFS

--- dataStructure/binarySearchTree.js
+++ dataStructure/binarySearchTree.js
@@ -53,6 +53,19 @@ class BinarySearchTree {
         if(!found) return undefined;
         return current;
     }
+    BFS() {
+        var data = [],
+            queue = [],
+            node = this.root;
+        queue.push(node);
+        while(queue.length) {
+            node = queue.shift()
+            data.push(node)
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return data;
+    }
 }
 
 var tree = new BinarySearchTree();
